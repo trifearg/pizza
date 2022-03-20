40855dd7e87c06f68a4dd957f7da6ade6ffec2bd
trifearg <trifearg@gmail.com>
2022-03-20 15:39:36 +0300

    update api

--- src/api/rest/pizza.api.ts
+++ src/api/rest/pizza.api.ts
@@ -1,14 +1,9 @@
-import axios from 'axios'
 import { PizzaModel, defaultPizza } from '../models/pizza.model'
 
 class PizzaApi {
     static async getPizza(): Promise<PizzaModel[]> {
-        let response = await axios.get('http://localhost:3000/data/pizza.json');
-        if (!response.data) {
-            response = await axios.put('http://localhost:3000/data/pizza.json', defaultPizza);
-        }
-
-        return response.data;
+        let response = await Promise.resolve(defaultPizza);
+        return response;
     }
 }
 
